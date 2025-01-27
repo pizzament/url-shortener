d729faa76cf5ea948b51fd062bc5360303b87dcc
pizzament <13290951+pizzament@users.noreply.github.com>
2025-01-27 20:17:10 +0300

    add slog, gitignore

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -1,20 +1,46 @@
 package main
 
 import (
-	"fmt"
+	"log/slog"
+	"os"
 	"url-shortener/internal/config"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
 )
 
 func main() {
 	cfg := config.MustLoad()
 
-	fmt.Println(cfg)
+	// fmt.Println(cfg)
 
-	// TODO: init logger: slog (import log/slog)
+	log := setupLogger(cfg.Env)
+
+	// log = log.With(slog.String("env", cfg.Env))
+	log.Info("Starting server...", slog.String("env", cfg.Env))
+	log.Debug("Debug started")
 
 	// TODO: init storage: sqlite
 
 	// TODO: init router: chi, render
 
 	// TODO: run server
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envDev:
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envProd:
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
+	}
+
+	return log
 }
